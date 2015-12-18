172478b02c6023839c0f15e7035ef017ad44ee47
ZS <pullingtab@gmail.com>
2015-12-18 11:32:59 +0800

    delete a file, add a file, modify a file

--- code.cpp
+++ code.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
+	int x,y;
+	x = 0;
+	y = 0;
+	if(x==0)
+		++y;
+
+	// return
 	return 0;
 }
 
---
+++ folder/codef.cpp
@@ -0,0 +1,12 @@
+# include <string>
+# include <iostream>
+
+int main()
+{
+	std::string tmp;
+	tmp = "Hello World!";
+	std::cout << tmp << '\n'; 
+	
+	return 0;
+}
+
